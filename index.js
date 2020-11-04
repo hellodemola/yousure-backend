d421d56db7725bc41282c98330d5cf2b72d9a857
Ademola Onasoga <ademola.onasoga@gmail.com>
2020-11-04 13:08:05 +0100

    added additional email to gmail

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const sanitizeHTML = require("sanitize-html");
 const jwt = require("jsonwebtoken");
 const sgMail = require('@sendgrid/mail');
 var nodemailer = require('nodemailer');
-let support = 'developer@ellopod.ng'
+let support = 'developer@ellopod.ng, ademola.onasoga@gmail.com'
 
 
 app.use(express.urlencoded({ extended: false }))
