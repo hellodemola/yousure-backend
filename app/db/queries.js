fdb39c1971dce429de88e8f25b849e4f8c87c7b3
Ademola Onasoga <ademola.onasoga@gmail.com>
2021-06-12 14:51:19 +0100

    ssl

--- app/db/queries.js
+++ app/db/queries.js
@@ -5,6 +5,7 @@ const pool = new Pool({
   database: process.env.DB_BASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_POST,
+  ssl: { require: false, rejectUnauthorized: false }
 })
 
 
