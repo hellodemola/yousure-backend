5f3ea216d6985c591d09bdcdb0d4f4b438740171
Ademola Onasoga <ademola.onasoga@gmail.com>
2021-06-12 14:09:47 +0100

    fix db details

--- app/db/queries.js
+++ app/db/queries.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'ademolaonasoga',
-  host: 'localhost',
-  database: 'yousure',
-  password: '',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_BASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_POST,
 })
 
 
--- config/env/production.js
+++ config/env/production.js
@@ -4,6 +4,7 @@ exports.default = {
     DATABASE_URL: process.env.DATABASE_URL,
     HOST: process.env.HOST, 
     PORT: process.env.PORT, 
-    API_VERSION: process.env.API_VERSION 
+    API_VERSION: process.env.API_VERSION,
+     
 
 }
